cd5c3c94671e2d23422d0570cbeea42956da58e8
Myka Kugaya <mykakug129@gmail.com>
2020-05-17 23:37:05 -0700

    array of employee data generated from init func

--- Develop/app.js
+++ Develop/app.js
@@ -4,25 +4,133 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+console.log("Please build your team.");
+
+const managerQues = [
+    {
+        type: "input",
+        message: "What is your manager's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your manager's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your manager's office number?",
+        name: "officeNumber"
+    }
+];
+const engineerQues = [
+    {
+        type: "input",
+        message: "What is your engineer's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's Github username?",
+        name: "github"
+    }
+];
+const internQues = [
+    {
+        type: "input",
+        message: "What is your intern's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your intern's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your intern's school?",
+        name: "school"
+    }
+];
+const nextEmployee = [
+    {
+        type: "list",
+        message: "Which team member would you like to add? (Use arrow keys)",
+        name: "role",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members."]
+    }
+];
+
+async function init() {
+    try {
+        // const employeesArr = [];
+        let currentObj = await inquirer.prompt(managerQues);
+        let role = "Manager";
+        const employeesArr = [{role, ...currentObj}];
+        console.log(employeesArr);
+        let next = await inquirer.prompt(nextEmployee);
+        while(next.role !== "I don't want to add any more team members.") {
+            switch (next.role) {
+                case "Engineer":
+                    currentObj = await inquirer.prompt(engineerQues);
+                    role = "Engineer";
+                    break;
+                case "Intern":
+                    currentObj = await inquirer.prompt(internQues);
+                    role = "Intern";
+                    break;
+            }
+            employeesArr.push({role, ...currentObj});
+            next = await inquirer.prompt(nextEmployee);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+init();
+// const html = render(employeesArr);
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
